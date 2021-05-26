b65c913dfdeb3858e640c044abaf8d7bcf137c9d
Thanglong2k <thanglong2k@gmail.com>
2021-05-26 16:22:49 +0700

    a

---
+++ OnlineShop/Areas/Admin/Controllers/UserGroupController.cs
@@ -0,0 +1,20 @@
+﻿using Model.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class UserGroupController : Controller
+    {
+        // GET: Admin/UserGroup
+        public ActionResult Index()
+        {
+            var dao = new UserGroupDao();
+            var model = dao.ListAllPaging();
+            return View(model);
+        }
+    }
+}
---
+++ OnlineShop/Model/Dao/UserGroupDao.cs
@@ -0,0 +1,22 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class UserGroupDao
+    {
+        OnlineShopDbContext db = null;
+        public UserGroupDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+        public IEnumerable<UserGroup> ListAllPaging()
+        {
+            return db.UserGroups.ToList();
+        }
+    }
+}
